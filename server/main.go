54fa4ea1bb47744fe65eaaff043d42e04727fb71
jsusmachaca <falcorgd@gmail.com>
2024-08-20 21:57:13 -0500

    create fetching data function

--- server/main.go
+++ server/main.go
@@ -5,23 +5,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strconv"
 
 	jsonResponse "github.com/jsusmachaca/goroapi/utils"
 )
 
-func sendData(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query().Get("page")
-	page := 1
-
-	if query != "" {
-		data, err := strconv.Atoi(query)
-		if err != nil {
-			panic(err)
-		}
-		page = data
-	}
-
+func getApiData(page int) []byte {
 	url := fmt.Sprintf("https://rickandmortyapi.com/api/character?page=%d", page)
 
 	res, err := http.Get(url)
@@ -34,18 +24,38 @@ func sendData(w http.ResponseWriter, r *http.Request) {
 
 	data, _ := io.ReadAll(res.Body)
 
+	return data
+}
+
+func sendData(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("page")
+	page := 1
+
+	if query != "" {
+		data, err := strconv.Atoi(query)
+		if err != nil {
+			panic(err)
+		}
+		page = data
+	}
+	data := getApiData(page)
+
 	var jsonRes jsonResponse.JsonResponse
 
 	error := json.Unmarshal(data, &jsonRes)
 
 	if error != nil {
-		w.Write([]byte("Hubo un error"))
+		w.Write([]byte(`{"message: "han error ocurred"}`))
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(jsonRes.Results)
 }
 
 func main() {
-	http.HandleFunc("/api/RnM", sendData)
+	PORT := fmt.Sprintf(":%s", os.Getenv("PORT"))
 
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/RnM", sendData)
+	http.ListenAndServe(PORT, nil)
 }
