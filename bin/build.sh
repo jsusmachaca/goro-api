c21c7c0695a57ffd2227b8e27ada3e9bca345d91
jsusmachaca <falcorgd@gmail.com>
2024-08-22 12:10:10 -0500

    update bash script

--- bin/build.sh
+++ bin/build.sh
@@ -5,9 +5,15 @@
 docker build -t apiserver server
 docker build -t apiclient client
 
-docker network create gogo
-
-docker run --name go-server -dp 8080:8080 --rm --network gogo -e PORT=8080 apiserver
-docker run --name express-client -dp 3000:3000 --rm --network gogo -e PORT=3000 -e API_SERVER=go-server:8080 apiclient
+docker run --name go-server -dp 8080:8080 --rm --network bridge -e PORT=8080 apiserver
+docker run --name express-client -dp 3000:3000 --rm --network bridge -e PORT=3000 -e API_SERVER=go-server:8080 apiclient
 
 docker cp go-server:/app/goroapi .
+
+printf "\e[H\e[2J\n"
+
+echo "Container created and listen on ports: "
+ports=$(docker ps | grep -o '0.0.0.0:[0-9]*')
+for port in $ports; do
+    echo "=> $port"
+done
